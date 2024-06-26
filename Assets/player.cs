17d7350819e6fce1ee6234d79207edd08f0125e9
KeiponNewLife <116721971+KeiponNewLife@users.noreply.github.com>
2024-06-26 14:58:35 +0900

    Add files via upload

--- Assets/player.cs
+++ Assets/player.cs
@@ -7,6 +7,11 @@ public class player : MonoBehaviour
     Transform myTransform;    // transform情報を格納する変数 
     Vector3 initial_position;     // 物体の初期位置を格納する変数
     //Vector3 initial_rotate;
+    int flag_t = 0;
+    int flag_m = 0;
+    int flag_d = 0;
+    int flag_v = 0;
+    int timer = 0;
     // Start is called before the first frame update 
     void Start() 
     { 
@@ -18,23 +23,71 @@ public class player : MonoBehaviour
  
     // Update is called once per frame 
     void Update() 
-    { 
-        if (Input.GetKey("up")) // ↑なら前（Z方向）に0.1だけ進む 
-        { 
-            transform.position += transform.forward * 0.1f; 
-        } 
-        if (Input.GetKey("down"))  // ↓なら-Z方向に0.1だけ進む 
-        { 
-            transform.position -= transform.forward * 0.1f; 
-        } 
-        if (Input.GetKey ("right"))   // ←ならY軸に3度回転する 
-        { 
-            transform.Rotate(0f,3.0f,0f); 
-        } 
-        if (Input.GetKey ("left"))   // →ならY軸に-3度回転する 
-        { 
-            transform.Rotate(0f, -3.0f, 0f); 
-        } 
+    {
+        if (flag_t == 1)
+        {
+            if (flag_d == 1)
+            {
+                if (Input.GetKey("up")) // ↑なら前（Z方向）に0.1だけ進む 
+                { 
+                    transform.position += transform.forward * 0.1f; 
+                } 
+                if (Input.GetKey("down"))  // ↓なら-Z方向に0.1だけ進む 
+                { 
+                    transform.position -= transform.forward * 0.1f; 
+                }
+            }
+            else if (flag_m == 1)
+            {
+
+            }
+            else if (flag_v == 1)
+            {
+                if (Input.GetKey("up")) // ↑なら前（Z方向）に0.1だけ進む 
+                { 
+                    transform.position += transform.forward * 0.2f; 
+                } 
+                if (Input.GetKey("down"))  // ↓なら-Z方向に0.1だけ進む 
+                { 
+                    transform.position -= transform.forward * 0.2f; 
+                } 
+                if (Input.GetKey ("right"))   // ←ならY軸に3度回転する 
+                { 
+                    transform.Rotate(0f,3.0f,0f); 
+                } 
+                if (Input.GetKey ("left"))   // →ならY軸に-3度回転する 
+                { 
+                    transform.Rotate(0f, -3.0f, 0f); 
+                }
+            }
+            timer += 1;
+            if (timer == 60 * 10)
+            {
+                flag_t = 0;
+                flag_m = 0;
+                flag_d = 0;
+                timer = 0;
+            }
+        }
+        else
+        {
+            if (Input.GetKey("up")) // ↑なら前（Z方向）に0.1だけ進む 
+            { 
+                transform.position += transform.forward * 0.1f; 
+            } 
+            if (Input.GetKey("down"))  // ↓なら-Z方向に0.1だけ進む 
+            { 
+                transform.position -= transform.forward * 0.1f; 
+            } 
+            if (Input.GetKey ("right"))   // ←ならY軸に3度回転する 
+            { 
+                transform.Rotate(0f,3.0f,0f); 
+            } 
+            if (Input.GetKey ("left"))   // →ならY軸に-3度回転する 
+            { 
+                transform.Rotate(0f, -3.0f, 0f); 
+            }
+        }
     }
     void OnCollisionEnter(Collision other)  // 衝突を判定する関数を呼ぶ 
     { 
@@ -42,6 +95,33 @@ public class player : MonoBehaviour
         { 
             this.transform.position = initial_position;
             //this.transform.Rotate(initial_rotate);
+        }
+
+        if (other.gameObject.name == "trap")  // 衝突した物体が「ゴール」なら（※） 
+        { 
+            int probability = Random.Range(0,4);
+            if (probability == 0)
+            {
+                this.transform.position = initial_position;
+                //this.transform.Rotate(initial_rotate);
+                Debug.Log("リスタート");
+            }
+            if (probability == 1)
+            {
+                flag_d = 1;
+                Debug.Log("特定の方向");
+            }
+            if (probability == 2)
+            {
+                flag_v = 1;
+                Debug.Log("速度2倍");
+            }
+            if (probability == 3)
+            {
+                flag_m = 1;
+                Debug.Log("動きを止める");
+            }
+            flag_t = 1;
         } 
     } 
 }
